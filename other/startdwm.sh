e6da46b7a76baf24a23229aa3a87668920ed3983
pauldub04 <paul.dubnyakov@gmail.com>
2024-08-17 19:40:24 +0300

    alttab and .xinitrc

--- other/startdwm.sh
+++ other/startdwm.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-#setxkbmap -option 'grp:rctrl_toggle' -layout 'us,ru'
 #setxkbmap us,ru -option grp:win_space_toggle
 #setxkbmap us,ru -option grp:alt_shift_toggle
-setxkbmap us,ru -option grp:rctrl_toggle
-setxkbmap -option caps:escape
+#setxkbmap us,ru -option grp:rctrl_toggle
+#setxkbmap -option caps:escape
 
-feh --bg-scale ~/Downloads/bg.png
+feh --bg-scale ~/Downloads/bg.jpg
 dwmblocks &
+flameshot &
+#lxsession &
+alttab -bg "#4a4a4a" -t 150x150 -i 150x64 -w 1 &
 
 while true; do
 	# with log
 	dwm 2> ~/.dwm.log
-
 	# no log
 	# dwm >/dev/null 2>&1
 
